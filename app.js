b844656b1442c163f69a3e9b69716f6caaf89394
Anil <kanilreddy867@gmail.com>
2024-03-27 13:18:06 +0530

    adjusted response of home route.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send(`
-          <h1>Welcome to the Quiz Application.</h1>
+          <h1>Welcome to the Pioneer Assignment.</h1>
     `);
 });
 
